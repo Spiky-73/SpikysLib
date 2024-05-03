9c9f1dbc0a3278f0aa0774839af4dfbaa8842c7f
Spiky-73 <cocojeanne73@gmail.com>
2024-05-03 09:53:12 +0200

    Snap function

--- MathX.cs
+++ MathX.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace SpikysLib;
 
@@ -9,12 +10,7 @@ public static class MathX {
     public static T Min<T>(params T[] values) where T : IComparable<T> => values.Min()!;
     public static T Max<T>(params T[] values) where T : IComparable<T> => values.Max()!;
 
-    public enum InclusionFlag {
-        Min = 0x01,
-        Max = 0x10,
-        Both = Min | Max
-    }
-
+    [Flags] public enum InclusionFlag { Min = 0x01, Max = 0x10, Both = Min | Max }
     public static bool InRange<T>(T self, T min, T max, InclusionFlag flags = InclusionFlag.Both) where T : IComparable<T> => InRange(Comparer<T>.Default, self, min, max, flags);
     public static bool InRange<T>(IComparer<T> comparer, T value, T min, T max, InclusionFlag flags = InclusionFlag.Both) {
         int l = comparer.Compare(value, min);
@@ -22,5 +18,21 @@ public static class MathX {
         return (l > 0 || (flags.HasFlag(InclusionFlag.Min) && l == 0)) && (r < 0 || (flags.HasFlag(InclusionFlag.Max) && r == 0));
     }
 
-    public static int GCD(int x, int y) => x == 0 ? y : GCD(y % x, x);
+    public enum SnapMode { Round, Ceiling, Floor }
+    public static T Snap<T>(T n, T step, SnapMode mode = SnapMode.Round) where T : INumber<T> {
+        T rem = n % step;
+        if (T.IsZero(rem)) return n;
+        switch (mode) {
+        case SnapMode.Floor:
+            return n - rem;
+        case SnapMode.Ceiling:
+            return n + step-rem;
+        default:
+            T extra = step - rem;
+            return n + (rem <= extra ? -rem : extra);
+        }
+    }
+
+
+    public static T GCD<T>(T x, T y) where T : INumber<T> => T.IsZero(x) ? y : GCD(y % x, x);
 }
--- SpikysLib.cs
+++ SpikysLib.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 
 namespace SpikysLib;
 
-
+// TODO custom float element (no slider -> text)
 public class SpikysLib : Mod {
 
     public override void Load() {
