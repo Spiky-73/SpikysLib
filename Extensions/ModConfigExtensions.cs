7fd1b7b2781770d745046e9b850e6e460990582a
Spiky-73 <cocojeanne73@gmail.com>
2024-06-23 13:09:04 +0200

    Moved Porting methods to a new class

--- Extensions/ModConfigExtensions.cs
+++ Extensions/ModConfigExtensions.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Reflection;
 using Terraria.ModLoader.Config;
 
@@ -11,29 +9,4 @@ public static class ModConfigExtensions {
     public static void Load(this ModConfig config) => Reflection.ConfigManager.Load.Invoke(config);
 
     public static void SetInstance(object instance, bool unload = false) => instance.GetType().GetField("Instance", BindingFlags.FlattenHierarchy | BindingFlags.Static | BindingFlags.Public)?.SetValue(null, unload ? null : instance);
-    
-    public static void MoveMember<TConfig>(bool cond, Action<TConfig> move) where TConfig: ModConfig => MoveMember(cond, c => move((TConfig)c));
-    public static void MoveMember(bool cond, Action<ModConfig> move) {
-        if (cond && LoadingConfig) _moves.Add(move);
-    }
-
-    internal static void HookPort(Action<ModConfig> orig, ModConfig config) {
-        LoadingConfig = true;
-        SaveLoadingConfig = false;
-        _moves.Clear();
-        orig(config);
-        LoadingConfig = false;
-
-        if (_moves.Count != 0) {
-            foreach (var m in _moves) m(config);
-            _moves.Clear();
-            SaveLoadingConfig = true;
-        }
-        if (SaveLoadingConfig) config.Save();
-        SaveLoadingConfig = false;
-    }
-
-    internal static bool LoadingConfig { get; private set; }
-    public static bool SaveLoadingConfig { get; set; }
-    private static readonly List<Action<ModConfig>> _moves = [];
 }
--- SpikysLib.cs
+++ SpikysLib.cs
@@ -1,15 +1,13 @@
 using SpikysLib.Configs.UI;
-using SpikysLib.Extensions;
 using Terraria.ModLoader;
 
 namespace SpikysLib;
 
-// TODO custom float element (no slider -> text)
 public class SpikysLib : Mod {
 
     public override void Load() {
 		MonoModHooks.Modify(Reflection.ConfigElement.DrawSelf, TextElement.ILTextColors);
-		MonoModHooks.Add(Reflection.ConfigManager.Load, ModConfigExtensions.HookPort);
+		MonoModHooks.Add(Reflection.ConfigManager.Load, PortConfig.HookPort);
 		Currencies.GetCurrencies();
 	}
 
