2528037e52b15439287e85e2cb6e6c6f77ffa4be
Spiky-73 <cocojeanne73@gmail.com>
2024-11-17 09:47:54 +0100

    added version next to obsolete items

--- Configs/UI/Buttons.cs
+++ Configs/UI/Buttons.cs
@@ -4,12 +4,12 @@ using ReLogic.Content;
 
 namespace SpikysLib.Configs.UI;
 
-[Obsolete("use SpikysLib.UI.Elements.HoverImage instead", true)]
+[Obsolete("use SpikysLib.UI.Elements.HoverImage instead", true)] // v1.3
 public sealed class HoverImage : global::SpikysLib.UI.Elements.HoverImage {
     public HoverImage(Asset<Texture2D> texture, string hoverText) : base(texture, hoverText) {}
 }
 
-[Obsolete("use SpikysLib.UI.Elements.HoverImageSplit instead", true)]
+[Obsolete("use SpikysLib.UI.Elements.HoverImageSplit instead", true)] // v1.3
 public sealed class HoverImageSplit : global::SpikysLib.UI.Elements.HoverImageSplit {
     public HoverImageSplit(Asset<Texture2D> texture, string hoverTextUp, string hoverTextDown) : base(texture, hoverTextUp, hoverTextDown) {}
 }
