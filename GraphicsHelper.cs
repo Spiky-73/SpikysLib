93a12f1614bc306b327145831d90ed7f0b3fa50a
Spiky-73 <cocojeanne73@gmail.com>
2024-12-04 23:52:14 +0100

    made DrawMouseText public

--- GraphicsHelper.cs
+++ GraphicsHelper.cs
@@ -35,8 +35,7 @@ public static class GraphicsHelper {
         }
     }
 
-
-    internal static void DrawMouseText(string tooltip) {
+    public static void DrawMouseText(string tooltip) {
         if (Main.gameMenu || Main.ingameOptionsWindow) Reflection.UIModConfig.Tooltip.SetValue(tooltip);
         else Main.instance.MouseText(tooltip);
     }
